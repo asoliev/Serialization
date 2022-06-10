1b2968c1755c0347390282318ec2aefb46701b87
v-asoliev <91322865+v-asoliev@users.noreply.github.com>
2022-06-11 00:21:34 +0500

    task2-own_serialialization

---
+++ OwnBinarySerialization/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OwnBinarySerialization;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+//using System.Runtime.Serialization.Formatters.Soap;
+
+string fileName = "dataStuff.myData";
+
+IFormatter formatter = new BinaryFormatter();
+//IFormatter formatter = new SoapFormatter();
+
+SerializeItem(fileName, formatter);
+DeserializeItem(fileName, formatter);
+
+void SerializeItem(string fileName, IFormatter formatter)
+{
+    SimpleClass t = new()
+    {
+        Id = 1,
+        Name = "Hello World"
+    };
+
+    using (FileStream fs = new(fileName, FileMode.Create))
+    {
+        formatter.Serialize(fs, t);
+    }
+}
+void DeserializeItem(string fileName, IFormatter formatter)
+{
+    using (FileStream fs = new FileStream(fileName, FileMode.Open))
+    {
+        SimpleClass t = (SimpleClass)formatter.Deserialize(fs);
+    }
+}
---
+++ OwnBinarySerialization/SimpleClass.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.Serialization;
+
+namespace OwnBinarySerialization
+{
+    [Serializable]
+    class SimpleClass : ISerializable
+    {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string? name;
+        public string? Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public SimpleClass() { }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("id", id, typeof(int));
+            info.AddValue("name", name, typeof(string));
+        }
+
+        public SimpleClass(SerializationInfo info, StreamingContext context)
+        {
+            object? id_obj = info.GetValue("id", typeof(int));
+            if (id_obj != null) id = (int)id_obj;
+
+            object? name_obj = info.GetValue("name", typeof(string));
+            if (name_obj != null) name = (string)name_obj;
+        }
+    }
+}
